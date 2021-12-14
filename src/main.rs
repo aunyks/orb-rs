48b15f72634173a53e8c1e7c953d017b5284181e
Nash <me@aunyks.com>
2021-12-14 15:17:39 -0500

    Add templating!

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use actix_files::Files;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
 use env_logger::{Builder, Env};
 use log::info;
+use tera::{Context, Tera};
 
 const BINDING_ADDRESS: &str = "localhost:8080";
 
@@ -26,6 +27,19 @@ async fn homepage() -> HttpResponse {
         .body(homepage_html)
 }
 
+async fn html_bye(req: HttpRequest) -> HttpResponse {
+    let name = req.match_info().query("name");
+    let bye_template = include_str!("../views/templates/bye.html");
+    let mut tera_context = Context::new();
+    tera_context.insert("name", name);
+    match Tera::one_off(bye_template, &tera_context, true) {
+        Err(..) => HttpResponse::InternalServerError().finish(),
+        Ok(interpolated_html) => HttpResponse::Ok()
+            .content_type("text/html; charset=UTF-8")
+            .body(interpolated_html),
+    }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // Initialize logger
@@ -46,6 +60,7 @@ async fn main() -> std::io::Result<()> {
             // Optionally restrict methods to a certain route
             // .route("/hey", web::post().to(|| HttpResponse::MethodNotAllowed())). By default they 404
             .route("/hey/{name}", web::get().to(hey_name))
+            .route("/bye/{name}", web::get().to(html_bye))
             // The serve_from arg (second arg) is the directory on system from which files will be served.
             // it's relative to the project root (wherever Cargo.toml is). The first arg is the server root path
             // from which files will be served
